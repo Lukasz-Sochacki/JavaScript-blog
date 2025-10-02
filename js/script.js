6cf9cde11d757afe61cfd52c22e7e5406dd8e417
Łukasz Sochacki <sochacki.luki@gmail.com>
2025-10-02 11:03:50 +0200

    Delete comments and install Eslint package

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@
   const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
-    //   console.log("Link was clicked!");
-    //   console.log(event);
 
     /* [DONE] remove class "active" from all article links */
     const activeLinks = document.querySelectorAll(".titles a.active");
@@ -14,8 +12,6 @@
     }
 
     /* [DONE] add class "active" to the clicked link */
-    //   console.log("clickedElement: ", clickedElement);
-    //   console.log("clickedElement (with plus): " + clickedElement);
     clickedElement.classList.add("active");
 
     /* [DONE] remove class "active" from all articles */
